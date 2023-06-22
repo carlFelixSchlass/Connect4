50f813f84396b160e4a38dab730b74f7826f393a
daimlereddi <carlf2004@web.de>
2023-06-22 14:15:13 +0200

    implement field array, getArray and printArray

--- GuiField.cpp
+++ GuiField.cpp
@@ -3,11 +3,12 @@
 //
 
 #include "GuiField.hpp"
+#include <iostream>
 
-void GuiField::printField() {
-     for (int i = 0, i < 3, i++){
-         K
+const void GuiField::printField() {
+     for (int i = 0; i < 3; i++){
+         for (int j = 0; j < 3; j++){
+             std::cout << Field::gameField[][];
+         }
      }
-
-
 }
