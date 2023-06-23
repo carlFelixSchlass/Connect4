1e19016acc327409fb7ba597bced2600a52cddb7
daimlereddi <carlf2004@web.de>
2023-06-23 13:11:26 +0200

    removed test outputs

--- main.cpp
+++ main.cpp
@@ -11,36 +11,6 @@ int main() {
     GuiField GUI;
     Field gameField;
 
-    for (int i = 0; i < 8; i++) {
-        gameField.layCoin(5 - i, 'i');
-        gameField.layCoin(6 - i, 'i');
-        gameField.layCoin(7 - i, 'i');
-        gameField.layCoin(8 - i, 'i');
-    }
-    for (int i = 0; i < 4; i++){
-        gameField.layCoin(5-i, 'i');
-        gameField.layCoin(6-i, 'i');
-        gameField.layCoin(7-i, 'i');
-        gameField.layCoin(8-i, 'i');
-    }
 
-        gameField.layCoin(2, 'o');
-        gameField.layCoin(3, 'o');
-        gameField.layCoin(4, 'o');
-        gameField.layCoin(5, 'o');
-
-
-
-
-    GUI.printField(gameField);
-
-
-
-    if (gameField.winCondition('x')){
-        std::cout << "Der Spieler mit dem Zeichen 'x' hat gewonnen!" << std::endl;
-    }
-    else if (gameField.winCondition('o')){
-        std::cout << "Der Spieler mit dem Zeichen '0' hat gewonnen!" << std::endl;
-    }
 
 }
