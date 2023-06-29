db997ccd8939ffa05f22a53602f58db7e7665884
MWirths <mattes.wirths@gmail.com>
2023-06-29 16:27:33 +0200

    1st ProBot Prototyp. Not working. Can be deleted

--- ProBot.hpp
+++ ProBot.hpp
@@ -7,6 +7,10 @@ class ProBot : public Player
 public:
 //Lays coin into the field with Pro Algorithm
     void layCoin(Field& field);
+    static int minimax(Field& field, std::vector<std::vector<char>>& board, int depth, bool isMaximizingPlayer, char maximizingPlayer, char minimizingPlayer, int ROWS, int COLS);
+    static bool isBoardFull(const std::vector<std::vector<char>>& board, int ROWS, int COLS);
+    static int evaluateBoard(const std::vector<std::vector<char>>& board, char player, int ROWS, int COLS);
+    static bool boardWin(std::vector<std::vector<char>>& field, char x, int ROW, int COLUMN);
 //Constructs ProBot (Same as player constructor) 
     ProBot(std::string name, char coin);  
     ~ProBot();
