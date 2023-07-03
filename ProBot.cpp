40a02b551065aace63c6994750b271258462610a
MWirths <mattes.wirths@gmail.com>
2023-07-03 15:04:43 +0200

    fix ProBot

--- ProBot.cpp
+++ ProBot.cpp
@@ -34,7 +34,6 @@ void ProBot::layCoin(Field& field)
     }
 
     int bestCol = ProBot::bestMove(field, m_Coin, player2);
-;
     field.layCoin(bestCol, m_Coin);
 }
 
@@ -43,7 +42,8 @@ int ProBot::bestMove(Field &field, char coin1, char coin2)
     int move = 4;
     int count = 0;
     std::vector<std::vector<char>> board = field.getField();
-    Field testfield = field;
+    Field testfield;
+    testfield = field;
 
     for (int i = 0; i < field.getRow(); i++)
     {
@@ -64,24 +64,20 @@ int ProBot::bestMove(Field &field, char coin1, char coin2)
         if(testfield.validMove(j))
         {
             testfield.layCoin(j, coin1);
-        }
-        else
-        {
-            break;
-        }
-        if(testfield.winCondition(coin1))
-        {
-            move = j;
-            return move;
-        }
-        testfield = field;
-        testfield.layCoin(j, coin2);
-        if(field.winCondition(coin2))
-        {
-            move = j;
-            return move;
-        }
-        testfield = field;
+            if(testfield.winCondition(coin1))
+            {
+                move = j;
+                return move;
+            }
+            testfield = field;
+            testfield.layCoin(j, coin2);
+            if(testfield.winCondition(coin2))
+            {
+                move = j;
+                return move;
+            }
+            testfield = field;
+         }
     }
     
     std::random_device rd;
@@ -90,13 +86,15 @@ int ProBot::bestMove(Field &field, char coin1, char coin2)
     do {
         std::uniform_int_distribution<int> distribution(1, field.getColumn());
         move = distribution(generator);
+        /*
         testfield = field;
         testfield.layCoin(move, coin1);
         testfield.layCoin(move, coin2);
-        if(testfield.winCondition(coin2))
-            {
+        if((testfield.winCondition(coin2)))
+        {
                 move = -1;
-            }
+        }
+        */
     } while (!(field.validMove(move)));
 
     return move;
